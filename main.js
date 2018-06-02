d10a9079b2fac5ec125b6d1b11d3cecd2f6a107b
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-06-02 15:37:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// Soon
+//server
+const app = this.app = express();
+app.use('/cat', catApi('/cat-api'));
+ 
+//client
+fetch('/cat').then((res) => res.json()).then(json => console.log(json)) 
+// {url: "/cat-api/images/PAY-Hero-Cat.jpg"}
