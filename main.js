5f41a773d6484783957d82b9a526b51b16f795af
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-06-02 15:48:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const catApi = require('cat-api');
 const app = this.app = express();
-app.use('/cat', catApi('/cat'));
+var cats = require("cats-js");
+var catapi = new cats();
  
-//client
-fetch('/cat').then((res) => res.json()).then(json => console.log(json)) 
-// {url: "/cat-api/images/PAY-Hero-Cat.jpg"}
+app.get("/json/cat", function (req, res){
+	catapi.get().then((cat) => {
+		res.send(cat);
+	});
+});
+
