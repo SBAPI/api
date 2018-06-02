b6fd63c3d9d8e0afff5ada300056e3333d53eb1b
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-06-02 16:09:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = this.app = express();
+var port = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
 var cats = require("cats-js");
 var catapi = new cats();
- 
-app.get("/cat", function (req, res){
-	catapi.get().then((cat) => {
-		res.send(cat);
-	});
-});
 
 app.get("/", function (req, res){
 	res.send(`{\n	"error": "Unknown api endpoint '/'"\n}`);
+}).get("/cat", function (req, res){
+	catapi.get().then((cat) => {
+		res.send(cat);
+	});
+}).listen(port, function() {
+	console.log("listening on port " + port);
 });
-
-var port = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
-app.listen(port);
-console.log("listening on port " + port);
